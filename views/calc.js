bff37fee3d5ff9b81687af201238e8dd7b979639
Matthew McGuff <matthewmcguff85@gmail.com>
2017-11-20 14:45:37 -0700

    Add js snippet to get dropdown values
    If I can get the correct dive to change with the values I select then we will be in great shape.

---
+++ views/calc.js
@@ -0,0 +1,9 @@
+function calculateRate()
+{
+
+  alert('working!');
+//   var postage = 1.00;
+
+// //alert('working');
+//   document.getElementById('price').innerHTML = '$' +postage.toFixed(2);
+} 
